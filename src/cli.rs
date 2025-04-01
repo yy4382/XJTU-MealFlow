d2225bae66e195d644686e1568534d0a7725bd43
Chris Yang <yy4382@outlook.com>
2025-04-01 16:49:31 +0800

    perf: update default tick and frame rates in CLI

--- src/cli.rs
+++ src/cli.rs
@@ -11,11 +11,11 @@ pub struct Cli {
     pub command: Option<Commands>,
 
     /// Tick rate, i.e. number of ticks per second
-    #[arg(short, long, value_name = "FLOAT", default_value_t = 4.0)]
+    #[arg(short, long, value_name = "FLOAT", default_value_t = 2.0)]
     pub tick_rate: f64,
 
     /// Frame rate, i.e. number of frames per second
-    #[arg(short, long, value_name = "FLOAT", default_value_t = 60.0)]
+    #[arg(short, long, value_name = "FLOAT", default_value_t = 30.0)]
     pub frame_rate: f64,
 
     /// Path to the data directory
